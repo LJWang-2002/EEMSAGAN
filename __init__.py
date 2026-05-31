d5c21a0db09661385aa5fa0d38f70b4618216e4e
Longjie Wang <1652361309@qq.com>
2026-05-31 16:39:42 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import importlib
-from basicsr.utils import scandir
-from os import path as osp
-from .eemsanet_arch import EEMSANet
-
-arch_folder = osp.dirname(osp.abspath(__file__))
-arch_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(arch_folder) if v.endswith('_arch.py')]
-_arch_modules = [importlib.import_module(f'eemsagan.archs.{file_name}') for file_name in arch_filenames]
